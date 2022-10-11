dc0a92e007c7cd7fc2664918c31c22986ca3df6d
Cameron Wright <cwright@nexient.com>
2022-10-11 13:36:59 -0700

    feat: main terraform

--- outputs.tf
+++ outputs.tf
@@ -12,6 +12,37 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-output "string" {
-  value = random_string.string.result
+output "dns_name" {
+  description = "ALB DNS Name"
+  value       = aws_lb.application.dns_name
+}
+
+output "zone_id" {
+  description = "ALB Zone ID"
+  value       = aws_lb.application.zone_id
+}
+
+output "arn" {
+  description = "ALB ARN"
+  value       = aws_lb_listener.application.arn
+}
+
+output "target_group_arn" {
+  description = "ALB Target Group ARN"
+  value       = aws_lb_target_group.application.arn
+}
+
+output "sg_id" {
+  description = "ALB Security Group, Allow 443"
+  value       = aws_security_group.load_balancer.id
+}
+
+output "sg_redirect_id" {
+  description = "ALB Allow port 80 redirect to 443"
+  value       = aws_security_group.load_balancer_redirect.id
+}
+
+output "application_sg_id" {
+  description = "Application Security Group"
+  value       = aws_security_group.application.id
 }
--- variables.tf
+++ variables.tf
@@ -12,17 +12,45 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-variable "length" {
-  type    = number
-  default = 24
+variable "name" {
+  description = "Name of the application"
+  type        = string
 }
 
-variable "number" {
-  type    = bool
-  default = true
+variable "naming_prefix" {
+  description = "Prefix to other resources"
 }
 
-variable "special" {
-  type    = bool
-  default = false
+variable "vpc_id" {
+  description = "Application Name running in ECS Task"
+  type        = string
+}
+
+variable "subnets" {
+  description = "Subnets for the Load Balancer to use"
+  type        = list(string)
+}
+
+variable "health_check_path" {
+  description = "Path to check App Health (e.g.; /, /health)"
+  type        = string
+  default     = "/"
+}
+
+variable "application_port" {
+  description = "Port of Application"
+  type        = string
+  default     = 80
+}
+
+variable "certificate_arn" {
+  description = "ARN of the ACM cert"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "Tags to be applied to all resources created"
+  type        = map(string)
+  default     = {}
 }
